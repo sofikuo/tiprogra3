4b83ce6be7179070f18a3e58814f2a608e378383
sofikuo <skuo@udesa.edu.ar>
2025-05-26 11:04:44 -0300

    crea

---
+++ trabajointegrador/src/screens/MiPerfil.js
@@ -0,0 +1,4 @@
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
+import React, {Component} from 'react'
+import { auth, db } from '../firebase/config'
+import Usuario from '../components/Usuario'
